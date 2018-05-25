630a0a398fff3fd9eef8441a651e2b4d3b6da275
William Kimball <wkkimball043@gmail.com>
2018-05-25 08:52:42 -0500

    made the switch statement more DRY, thanks Emily

--- grades.js
+++ grades.js
@@ -10,20 +10,20 @@ let grades = {
 }
 //for loop to iterate over each index in scores
 for (let i = 0; i < scores.length; i++) {
-    let test = scores[i];
+    // let test = scores[i];
 
     //check each grade, and if there is a match, increment by one
-    switch (test) {
-        case 91: case 92: case 93: case 94: case 95: case 96: case 97: case 98: case 99: case 100:
+    switch (true) {
+        case (scores[i] >= 91 && scores[i] <= 100):
             grades.a++
             break;
-        case 81: case 82: case 83: case 84: case 85: case 86: case 87: case 88: case 89: case 90:
+        case (scores[i] >= 81 && scores[i] <= 90):
             grades.b++
             break;
-        case 71: case 72: case 73: case 74: case 75: case 76: case 77: case 78: case 79: case 80:
+        case (scores[i] >= 71 && scores[i] <= 80):
             grades.c++
             break;
-        case 61: case 62: case 63: case 64: case 65: case 66: case 67: case 68: case 69: case 70:
+        case (scores[i] >= 61 && scores[i] <= 70):
             grades.d++
             break;
         default:
